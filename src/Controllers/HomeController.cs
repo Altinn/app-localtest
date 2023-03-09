2e1ed614aa412e67368c8a44326fcc2786db242d
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-03-09 08:09:59 +0100

    Reverted appsettings and updated accessmanagement redirect url

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -155,7 +155,7 @@ namespace LocalTest.Controllers
 
             if (startAppModel.AppPathSelection?.Equals("accessmanagement") == true)
             {
-                return Redirect($"http://am.ui.local.altinn.cloud/accessmanagement/ui/given-api-delegations/overview");
+                return Redirect($"http://am.ui.local.altinn.cloud/accessmanagement/ui/offered-api-delegations/overview");
             }
 
             Application app = await _localApp.GetApplicationMetadata(startAppModel.AppPathSelection);
@@ -410,7 +410,7 @@ namespace LocalTest.Controllers
         }
 
         private async Task<List<SelectListItem>> GetAppsList()
-        {
+        {           
             var applications = await _localApp.GetApplications();
             return applications.Select((kv) => GetSelectItem(kv.Value, kv.Key)).ToList();
         }
