ed845c9128b76a27a33e48453359fa70da1e43b4
acn-sbuad <stephanie.buadu@avanade.com>
2023-06-06 16:15:59 +0200

    Added support for refences

--- src/Services/Storage/Implementation/DataRepository.cs
+++ src/Services/Storage/Implementation/DataRepository.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 using Altinn.Platform.Storage.Interface.Models;
 using Altinn.Platform.Storage.Repository;
