f6569c7a88e20e50ccfaabf72fb5c55136c73187
acn-sbuad <stephanie.buadu@avanade.com>
2023-06-08 17:33:23 +0200

    removed obsolete using

--- src/Services/Storage/Implementation/DataRepository.cs
+++ src/Services/Storage/Implementation/DataRepository.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Threading.Tasks;
 
 using Altinn.Platform.Storage.Interface.Models;
 using Altinn.Platform.Storage.Repository;
