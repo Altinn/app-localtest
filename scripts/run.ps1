62748880c4415430abc0dd239c21eb56d93919d9
Martin Othamar <martin.othamar@digdir.no>
2024-05-16 18:03:30 +0200

    Prefer docker-compose over podman-compose due to --profile support being relatively recent in podman

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -9,6 +9,14 @@ if ($args[0] -eq "stop")
         Write-host "Stopping using docker"
         docker compose --profile "*" down -v
     }
+    elseif (Get-Command "docker-compose" -errorAction SilentlyContinue)
+    {
+        # If the user is not using docker, there should be podman installed
+        # If additionally docker-compose is installed, we use that since it has had '--profile' support for a long time,
+        # whereas podman-compose has only recently added support, and not many users have the latest versions
+        Write-host "Stopping using docker-compose"
+        docker-compose --file podman-compose.yml --profile "*" down -v
+    }
     elseif (Get-Command "podman" -errorAction SilentlyContinue)
     {
         Write-host "Stopping using podman"
@@ -28,6 +36,15 @@ else
         Write-host "Running using docker"
         docker compose --profile "*" down -v
         docker compose --profile "*" up -d --build
+    }
+    elseif (Get-Command "docker-compose" -errorAction SilentlyContinue)
+    {
+        # If the user is not using docker, there should be podman installed
+        # If additionally docker-compose is installed, we use that since it has had '--profile' support for a long time,
+        # whereas podman-compose has only recently added support, and not many users have the latest versions
+        Write-host "Running using docker-compose"
+        docker-compose --file podman-compose.yml --profile "*" down -v
+        docker-compose --file podman-compose.yml --profile "*" up -d --build
     }
     elseif (Get-Command "podman" -errorAction SilentlyContinue)
     {
--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,12 @@ if [ $1 = "stop" ]; then
     if [ -x "$(command -v docker)" ]; then
         echo "Stopping using docker"
         docker compose --profile "*" down -v
+    elif [ -x "$(command -v docker-compose)" ]; then
+        # If the user is not using docker, there should be podman installed
+        # If additionally docker-compose is installed, we use that since it has had '--profile' support for a long time,
+        # whereas podman-compose has only recently added support, and not many users have the latest versions
+        echo "Stopping using docker-compose"
+        docker-compose --file podman-compose.yml --profile "*" down -v
     elif [ -x "$(command -v podman)" ]; then
         echo "Stopping using podman"
         podman compose --file podman-compose.yml --profile "*" down -v
@@ -20,6 +26,13 @@ else
         echo "Running using docker"
         docker compose --profile "*" down -v
         docker compose --profile "*" up -d --build
+    elif [ -x "$(command -v docker-compose)" ]; then
+        # If the user is not using docker, there should be podman installed
+        # If additionally docker-compose is installed, we use that since it has had '--profile' support for a long time,
+        # whereas podman-compose has only recently added support, and not many users have the latest versions
+        echo "Running using docker-compose"
+        docker-compose --file podman-compose.yml --profile "*" down -v
+        docker-compose --file podman-compose.yml --profile "*" up -d --build
     elif [ -x "$(command -v podman)" ]; then
         echo "Running using podman"
         podman compose --file podman-compose.yml --profile "*" down -v
