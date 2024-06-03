a680417d93e042b8c3f74482787b87c641ead987
Bjørnar Østtveit <bjosttveit@gmail.com>
2024-06-03 12:34:20 +0200

    workaround for podman host-gateway timeout when selecting frontend version

--- src/Services/LocalFrontend/Implementation/LocalFrontendService.cs
+++ src/Services/LocalFrontend/Implementation/LocalFrontendService.cs
@@ -3,45 +3,52 @@ using LocalTest.Configuration;
 using LocalTest.Models;
 using LocalTest.Services.LocalFrontend.Interface;
 using Microsoft.Extensions.Options;
+using static System.Linq.Enumerable;
 
 namespace LocalTest.Services.LocalFrontend;
 
-public class LocalFrontendService: ILocalFrontendService
+public class LocalFrontendService : ILocalFrontendService
 {
     private readonly HttpClient _httpClient;
     private static readonly Range PortRange = 8080..8090;
     private readonly string _localFrontedBaseUrl;
 
-    public LocalFrontendService(IHttpClientFactory httpClientFactory, IOptions<LocalPlatformSettings> localPlatformSettings)
+    public LocalFrontendService(
+        IHttpClientFactory httpClientFactory,
+        IOptions<LocalPlatformSettings> localPlatformSettings
+    )
     {
         _httpClient = httpClientFactory.CreateClient();
-        _localFrontedBaseUrl = $"{localPlatformSettings.Value.LocalFrontendProtocol}://{localPlatformSettings.Value.LocalFrontendHostname}";
+        _httpClient.Timeout = TimeSpan.FromMilliseconds(500);
+        _localFrontedBaseUrl =
+            $"{localPlatformSettings.Value.LocalFrontendProtocol}://{localPlatformSettings.Value.LocalFrontendHostname}";
     }
 
     public async Task<List<LocalFrontendInfo>> GetLocalFrontendDevPorts()
     {
-        var ports = new List<LocalFrontendInfo>();
-        for (int i = PortRange.Start.Value; i < PortRange.End.Value; i++)
+        var tasks = Range(PortRange.Start.Value, PortRange.End.Value).Select(port => TestFrontendDevPort(port));
+        var result = await Task.WhenAll(tasks);
+        return result.Where(x => x != null).Select(x => x!.Value).ToList();
+    }
+
+    private async Task<LocalFrontendInfo?> TestFrontendDevPort(int port)
+    {
+        try
         {
-            try
+            var response = await _httpClient.GetAsync($"{_localFrontedBaseUrl}:{port.ToString()}/");
+            if (response.Headers.TryGetValues("X-Altinn-Frontend-Branch", out var values))
             {
-                var response =
-                    await _httpClient.GetAsync($"{_localFrontedBaseUrl}:{i.ToString()}/");
-                if (response.Headers.TryGetValues("X-Altinn-Frontend-Branch", out var values))
+                return new LocalFrontendInfo()
                 {
-                    
-                    ports.Add(new LocalFrontendInfo()
-                    {
-                        Port = i.ToString(),
-                        Branch = values?.First() ?? "Unknown"
-                    });
-                }
-            }
-            catch(HttpRequestException)
-            {
-                
+                    Port = port.ToString(),
+                    Branch = values?.First() ?? "Unknown"
+                };
             }
         }
-        return ports;
+        catch (TaskCanceledException) { }
+        catch (HttpRequestException) { }
+
+        return null;
     }
 }
+
