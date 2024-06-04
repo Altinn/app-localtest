959c2f7fa578f4b031c984e8d8d46c5724d203b0
acn-sbuad <stephanie.buadu@avanade.com>
2024-06-04 16:08:38 +0200

    current progress

--- src/Notifications/LocalTestNotifications/LocalProfileClient.cs
+++ src/Notifications/LocalTestNotifications/LocalProfileClient.cs
@@ -1,13 +1,48 @@
 ﻿using Altinn.Notifications.Core.Integrations;
 using Altinn.Notifications.Core.Models.ContactPoints;
+using Altinn.Platform.Profile.Models;
+
+using LocalTest.Services.Register.Interface;
+using LocalTest.Services.TestData;
 
 namespace LocalTest.Notifications.LocalTestNotifications
 {
     public class LocalProfileClient : IProfileClient
     {
-        public Task<List<UserContactPoints>> GetUserContactPoints(List<string> nationalIdentityNumbers)
+        private readonly TestDataService _testDataService;
+
+        public LocalProfileClient(TestDataService testDataService)
         {
-            throw new NotImplementedException();
+            _testDataService = testDataService;
+        }
+
+        public async Task<List<UserContactPoints>> GetUserContactPoints(List<string> nationalIdentityNumbers)
+        {
+            List<UserContactPoints> contactPoints = new();
+            var data = await _testDataService.GetTestData();
+
+            List<UserProfile> users = new();
+
+            foreach (var item in data.Profile.User)
+            {
+                users.AddRange(data.Profile.User
+                    .Where(u => nationalIdentityNumbers.Contains(u.Value.Party.SSN))
+                    .Select(u => u.Value)
+                    .ToList());
+            }
+
+            foreach (UserProfile user in users)
+            {
+                contactPoints.Add(new UserContactPoints()
+                {
+                    NationalIdentityNumber = user.Party.SSN,
+                    Email = user.Email,
+                    MobileNumber = user.PhoneNumber
+                });
+            }
+
+            return contactPoints;
+
         }
 
         public Task<List<OrganizationContactPoints>> GetUserRegisteredOrganizationContactPoints(string resourceId, List<string> organizationNumbers)
