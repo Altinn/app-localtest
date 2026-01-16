e0e3cbab5b0354e9fc2277ca0e83fdb3c818dd3c
Bjørn Tore Gjerde <btgjerde@gmail.com>
2026-01-16 12:32:29 +0100

    Fix weird DI registration.

--- src/Startup.cs
+++ src/Startup.cs
@@ -95,8 +95,7 @@ namespace LocalTest
             services.AddSingleton<IClaims, ClaimsService>();
             services.AddSingleton<IInstanceRepository, InstanceRepository>();
             services.AddSingleton<IInstanceAndEventsRepository, InstanceAndEventsRepository>();
-            services.AddSingleton<DataRepository>();
-            services.AddSingleton<IDataRepository>(sp => sp.GetRequiredService<DataRepository>());
+            services.AddSingleton<IDataRepository, DataRepository>();
             services.AddSingleton<IBlobRepository, BlobRepository>();
             services.AddSingleton<IInstanceEventRepository, InstanceEventRepository>();
             services.AddSingleton<IEventsRepository, EventsRepository>();
